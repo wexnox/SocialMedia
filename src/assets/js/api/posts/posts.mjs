f658759b1b681b6c9e8b7af19db11c8ca713e795
Lasse <lasseronnlie@hotmail.com>
2023-05-21 02:37:07 +0200

    *

--- src/assets/js/api/posts/posts.mjs
+++ src/assets/js/api/posts/posts.mjs
@@ -2,9 +2,7 @@ import { API_SOCIAL_URL } from "../api/constants.mjs";
 import { authFetch } from "../api/authFetch.mjs";
 
 export async function fetchAllPosts(getPostUrl) {
-
   const url = `${API_SOCIAL_URL}/posts/?sort=created&sortOrder=desc&_author=true&_reactions=true&_comments=true`;
-
 
   const method = "GET";
 
@@ -13,13 +11,9 @@ export async function fetchAllPosts(getPostUrl) {
     const posts = await response.json(getPostUrl);
     console.log(posts);
     // renderPosts(posts);
-
   } catch (error) {
-
     console.log(error);
-
   }
-
 }
 
 fetchAllPosts();
