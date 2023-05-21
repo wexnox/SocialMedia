cd3a20820c798460432074d5e3041d4c7b9b7939
Lasse <lasseronnlie@hotmail.com>
2023-05-21 03:54:18 +0200

    filter seach buggy wooggy

--- src/assets/js/handlers/filter.mjs
+++ src/assets/js/handlers/filter.mjs
@@ -5,11 +5,12 @@ import { renderPosts} from "../components/renderPosts.mjs";
 
 export async function asceFilter(){
     const container = document.querySelector("#postContainer");
+    const ascendingFilter = document.querySelector("#asceFilter");
 
 }
 
 
 export async function descFilter() {
     const container = document.querySelector("#postContainer");
-
+    const filterOldest = document.querySelector("#descFilter");
 }
