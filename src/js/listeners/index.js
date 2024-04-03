a7d4b03bcfb207e431f9abea79056e56eb12b73a
Lasse R <lasseronnlie@hotmail.com>
2024-04-03 09:02:05 +0200

    Add event handlers and listeners for post and profile updates

--- src/js/listeners/index.js
+++ src/js/listeners/index.js
@@ -1,10 +1,2 @@
 export * from "./auth/index.js";
-
-
-// export * from "./posts/createPost.mjs";
-
-
-
-// export * from "./posts/updatePost.mjs";
-// export * from "./profile/updateProfile.mjs";
-// export * from "./filter.mjs";
+export * from './posts/index.js'
