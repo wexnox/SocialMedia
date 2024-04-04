7ad97c498931f5b2efa57741284ea7763121a970
Lasse R <lasseronnlie@hotmail.com>
2024-04-04 12:53:59 +0200

    Refactor the `registerHandler` function in the authentication API to handle registration errors and display appropriate messages.

--- src/js/api/auth/registerHandler.js
+++ src/js/api/auth/registerHandler.js
@@ -1,19 +1,28 @@
 import { apiPath } from "../constants.mjs";
 import { headers } from "../headers.js";
+import { displayAlertMessage } from "@/js/ui/common/displayMessage.js";
 
-export async function registerHandler(name, email, password, avatar) {
+export async function registerHandler(name, email, password, banner, avatar) {
 
   const response = await fetch(`${apiPath}/auth/register`, {
     headers: headers("application/json"),
     method: "post",
-    body: JSON.stringify({ name, email, password, avatar }),
+    body: JSON.stringify({ name, email, password, banner, avatar }),
   })
 
+  if (!response.ok) {
+    const errorDetail = await response.json();
+    console.error('Error detail:', errorDetail);
+  }
+
   if (response.ok){
-    const result = await response.json()
-    console.log(result);
-    alert("You are now registered")
-    return result
+    const profile = await response.json()
+    console.log(profile);
+    displayAlertMessage('success', 'You are now registered');
+    return profile
+  } else {
+    displayAlertMessage('danger', 'Unexpected error occurred. Please try again later.');
+
   }
 
 }
