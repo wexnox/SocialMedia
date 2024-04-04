2fad61f1f5e6417830ce02a136ce8ca451f00666
Lasse R <lasseronnlie@hotmail.com>
2024-04-04 12:55:16 +0200

    Refactor setRegisterFormListener and handle registration errors

--- src/js/listeners/auth/setRegisterFormListener.js
+++ src/js/listeners/auth/setRegisterFormListener.js
@@ -1,25 +1,33 @@
-import * as auth from "../../api/auth/index.js";
+import { registerHandler } from "@/js/api/auth/index.js";
+import { displayAlertMessage } from "@/js/ui/common/displayMessage.js";
 
-export async function setRegisterFormListener(event) {
-  event.preventDefault();
-  const form = event.target;
-  const formData = new FormData(form);
-  const email = formData.get("email");
-  const name = formData.get("name");
-  const password = formData.get("password");
-  const avatar = formData.get("avatar");
 
-  try {
-    await auth.registerHandler(name, email, password, avatar);
-  } catch {
-    return alert("There was a problem creating your account");
+export async function setRegisterFormListener() {
+  const form = document.getElementById("registerForm");
+
+  if (form) {
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      console.log('Form submission intercepted');
+      const form = event.target;
+      const formData = new FormData(form);
+      const email = formData.get("email");
+      const name = formData.get("name");
+      const password = formData.get("password");
+      const banner = formData.get("banner")
+      const avatar = formData.get("avatar");
+
+      try {
+        console.log(name, email, password);
+        console.log(JSON.stringify(name, email, password));
+        await registerHandler(name, email, password, banner, avatar);
+
+      } catch {
+        displayAlertMessage("danger", "There was a problem creating your account");
+      }
+    });
+
   }
 
-  try {
-    await auth.loginHandler(email, password);
-    location.reload();
-  } catch {
-    return alert("There was a problem logging into your new account");
-  }
 
 }
