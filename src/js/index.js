beae56fba1dc980b7a15152e98e53c55ed72bb43
Lasse R <lasseronnlie@hotmail.com>
2024-04-04 12:51:14 +0200

    Add eventListener to buildMenu and router function

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-
+document.addEventListener('DOMContentLoaded', () => {
 buildMenu();
 router();
+});
