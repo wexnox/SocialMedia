16952b364fd65d6821ce73f6f4034dfe1d640d58
Lasse R <lasseronnlie@hotmail.com>
2024-04-04 12:53:11 +0200

    Update logout process and add alert message

--- src/js/api/auth/logoutHandler.js
+++ src/js/api/auth/logoutHandler.js
@@ -1,9 +1,11 @@
 import { clearTokenFromStorage } from "../../storage/index.js";
 import buildMenu from "../../ui/common/buildMenu.js";
+import { displayAlertMessage } from "@/js/ui/common/displayMessage.js";
+
 
 export function logoutHandler() {
-	clearTokenFromStorage("token");
+	clearTokenFromStorage("accessToken");
 	clearTokenFromStorage("profile");
-	// location.href = '/';
 	buildMenu(); // Rebuild the menu to reflect the unauthenticated state
+	displayAlertMessage('success', 'You are now logged out');
 }
