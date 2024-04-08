50049f2e8c25999c902198bfe40e804d250129be
Lasse R <lasseronnlie@hotmail.com>
2024-04-08 14:59:44 +0200

    Refactor code to use renderDetailsPost function

--- src/js/api/posts/getPostDetailsHandler.js
+++ src/js/api/posts/getPostDetailsHandler.js
@@ -1,5 +1,6 @@
 import { apiPath } from "../constants.mjs";
 import { headers } from "@/js/api/headers.js";
+import { renderDetailsPost } from "@/js/components/renderDetailsPost.js";
 
 let postContainer;
 
@@ -37,47 +38,9 @@ export async function getPostDetailsHandler() {
     const result = await response.json();
     console.log(result);
     document.title = result.title;
-
-    createHtml(result);
+    renderDetailsPost(result, postContainer);
   } catch(error) {
     console.error("Error fetching post details:", error);
   }
 }
 
-function createHtml(result) {
-  const tagList = result.tags.map(tag => `<li>${tag}</li>`).join('');
-  let authorName = result.author ? result.author.name : '';
-  let authorEmail = result.author ? result.author.email : '';
-  postContainer.innerHTML += `
-        <div class="card-head ">
-            <h1>Title: ${result.title}</h1>
-            <h2>Created by: ${authorName}</h2>
-            <h3>Email: ${authorEmail}</h3>
-            <h4>Created at: ${result.created}</h4>
-            <h4>Updated at: ${result.updated}</h4>
-            <h4>Tags:</h4>
-            <ul>${tagList}</ul>
-        </div>
-        <div class="card-body">
-            ${result.body}
-        </div>
-    `;
-  if (result.media) {
-    postContainer.innerHTML += `<img src="${result.media}" alt="Post media">`;
-  }
-  if (result.reactions && result.reactions.length > 0) {
-    let reactionsHTML = result.reactions.map(reaction => `<li>${reaction.symbol} ${reaction.count}</li>`).join('');
-    postContainer.innerHTML += `
-            <h4>Reactions:</h4>
-            <ul>${reactionsHTML}</ul>
-        `;
-  }
-  if (result.comments && result.comments.length > 0) {
-    let commentsHTML = result.comments.map(comment => `<li>${comment.body} by ${comment.author.name}</li>`).join('');
-    postContainer.innerHTML += `
-            <h4>Comments:</h4>
-            <ul>${commentsHTML}</ul>
-        `;
-  }
-  postContainer.innerHTML += `<h4>Reaction count: ${result._count.reactions}, Comment count: ${result._count.comments}</h4>`;
-}
