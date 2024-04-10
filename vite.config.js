0d05fb9851669c63cae002f47d2d441b4230f201
Lasse R <lasseronnlie@hotmail.com>
2024-04-10 11:11:31 +0200

    Update paths and add new routes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,9 +15,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: path.resolve(__dirname, 'index.html'),
-        login: path.resolve(__dirname, 'pages/auth/login.html'),
-        register: path.resolve(__dirname, 'pages/auth/register.html'),
-
+        login: path.resolve(__dirname, 'auth/login.html'),
+        createPost: path.resolve(__dirname, 'pages/posts/create.html'),
+        editPost: path.resolve(__dirname, 'pages/posts/edit.html'),
+        listMyPosts: path.resolve(__dirname, 'pages/posts/index.html'),
+        postDetails: path.resolve(__dirname, 'pages/posts/postDetails.html'),
 
       },
     },
