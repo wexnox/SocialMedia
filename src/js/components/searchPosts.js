35b3a513bee664514fa8b9b5c96df79ec19ccdb0
Lasse R <lasseronnlie@hotmail.com>
2024-04-10 10:51:34 +0200

    Refactor searchPosts function

--- src/js/components/searchPosts.js
+++ src/js/components/searchPosts.js
@@ -1,22 +1,3 @@
-import {renderPosts} from "./renderPosts.mjs"
-
-export function search(post) {
-    const search = document.querySelector('#search')
-
-    search.onkeyup = function (event) {
-        console.log(event);
-        const searchInput = event.target.value.trim().toLowerCase();
-
-        const convertedPosts = post.filter((post) => {
-            if (post.title.toLowerCase().includes(searchInput)) {
-                return true;
-            } else if (post.author.name.toLowerCase().includes(searchInput)) {
-                return true;
-            }
-
-        })
-
-        renderPosts(convertedPosts);
-        console.log(convertedPosts)
-    }
+export function searchPosts(posts, searchTerm) {
+    return posts.filter(post => post.title.toLowerCase().includes(searchTerm) || post.body.toLowerCase().includes(searchTerm));
 }
