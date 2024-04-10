f760c19e5c77e75e7803c6b82dd3234d91d58108
Lasse R <lasseronnlie@hotmail.com>
2024-04-10 10:47:49 +0200

    Remove profile related files and refactor filter functionality

--- src/js/components/filterPosts.mjs
+++ src/js/components/filterPosts.mjs
@@ -1,22 +1,22 @@
-import {API_SOCIAL_URL} from "../api/constants.mjs";
-import {authFetch} from "../api/authFetch.mjs";
-import {renderPosts} from "../components/renderPosts.mjs";
+import { apiPath } from "../api/constants.mjs";
+import {renderPosts} from "./renderPosts.mjs";
+import { headers } from "@/js/api/headers.js";
 
 export async function asceFilter() {
     const container = document.querySelector("#postsContainer");
     const ascendingFilter = document.querySelector("#asceFilter");
 
     try {
-        const ascePostsUrl = API_SOCIAL_URL + "/posts?_author=true&sort=updated&sortOrder=asc";
-        const response = await authFetch(ascePostsUrl)
+        const ascePostsUrl = apiPath + "/posts?_author=true&sort=updated&sortOrder=asc";
+        const response = await fetch(ascePostsUrl, { headers: headers('application/json') });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const post = await response.json();
 
         ascendingFilter.addEventListener("click", () => {
-
             container.innerHTML = "";
-
             renderPosts(post, container);
-
         });
     } catch (error) {
         console.log(error)
@@ -29,15 +29,15 @@ export async function descFilter() {
     const decendingFilter = document.querySelector("#descFilter");
 
     try {
-
-        const descPostsUrl = API_SOCIAL_URL + "/posts?_author=true&sort=updated&sortOrder=desc";
-        const response = await authFetch(descPostsUrl)
+        const descPostsUrl = apiPath + "/posts?_author=true&sort=updated&sortOrder=desc";
+        const response = await fetch(descPostsUrl, { headers: headers('application/json') });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const post = await response.json()
 
         decendingFilter.addEventListener("click", () => {
-
             container.innerHTML = "";
-
             renderPosts(post, container)
         });
     } catch (error) {
