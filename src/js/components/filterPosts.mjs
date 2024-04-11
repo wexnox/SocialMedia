eea1402d36abfbf1f5811f1a82ce4612deaffebd
Lasse R <lasseronnlie@hotmail.com>
2024-04-11 22:56:55 +0200

    Change default post sorting order

--- src/js/components/filterPosts.mjs
+++ src/js/components/filterPosts.mjs
@@ -2,7 +2,7 @@ import { apiPath } from "../api/constants.mjs";
 import { headers } from "@/js/api/headers.js";
 
 export async function fetchAndSortPosts(sortOrder = 'asc') {
-    const sortedPostsUrl = `${apiPath}/posts?_author=true&sort=updated&sortOrder=${sortOrder}`;
+    const sortedPostsUrl = `${apiPath}/posts?_author=true&sort=updated&sortOrder=${sortOrder}&`;
     const response = await fetch(sortedPostsUrl, { headers: headers('application/json') });
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
