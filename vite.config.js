be374aced1cfad3f3693a94728589d8653fd68a5
Lasse R <lasseronnlie@hotmail.com>
2025-01-09 17:54:31 +0100

    configure SCSS in Vite.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/global.scss";`
+        additionalData: `@import "bootstrap/scss/bootstrap";`
       }
     }
   },
